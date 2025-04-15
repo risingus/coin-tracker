101331a9d0131e1b008203cc9e0e51597b8f54da
rising-gus <gustavo.it@outlook.com>
2025-04-15 07:23:13 -0300

    feat - modal
    1. starting development of modal

--- src/components/AddCurrencyModal.vue
+++ src/components/AddCurrencyModal.vue
@@ -1,7 +1,11 @@
 <script lang='ts' setup>
 import { ref } from 'vue'
+import { useCurrencies } from '../composables/useCurrencies'
+import Select from './Select.vue'
 
   const show = ref(false)
+
+  const {data: currencies} = useCurrencies()
 
   function handleModal() {
     show.value = !show.value
@@ -12,7 +16,27 @@ import { ref } from 'vue'
 <template>
 
   <div v-if="show" class="modal-overlay">
-    'blablas'
+    <div class='modal-content'>
+     <div class='modal-header'>
+        <strong>
+          Add Currency
+        </strong>
+        <button>
+          x
+        </button>
+     </div>
+
+     <div class='modal-section'>
+        <Select />
+
+     </div>
+
+
+     <div class='modal-footer'>
+      
+     </div>
+
+    </div>
 
   </div>
 
@@ -27,7 +51,27 @@ import { ref } from 'vue'
 
 <style scoped>
   .modal-overlay {
-    
+    background-color: rgba(0, 0, 0, 0.6);
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    .modal-content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      background-color: red;
+
+    }
   }
 
 </style>
---
+++ src/components/Select.vue
@@ -0,0 +1,30 @@
+<script lang='ts' setup>
+  import { useCurrencies } from '../composables/useCurrencies'
+  const { data: currencies } = useCurrencies()
+</script>
+
+<template>
+  <select class='select'>
+    <option v-for='currency in currencies' :value='currency'
+      class='select-option'>
+      {{ `${currency.code} - ${currency.name}` }}
+    </option>
+  </select>
+</template>
+
+
+<style scoped>
+
+.select {
+  width: 100%;
+  padding: 0.5rem;
+  border-radius: 5px;
+
+  &,
+  &::picker(select) {
+    appearance: base-select;
+    padding: 0.5rem;
+  }
+
+}
+</style>

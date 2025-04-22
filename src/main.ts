b12311706b6d120c625462c0f81da40195b20ff5
Gustavo Lima <gustavo.lima@nd.com.br>
2025-04-22 16:24:54 -0300

    feat - relative path, remove pinia
    1. Add relative path config.
    2. Remove pinia.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 import './style.css'
 import App from './App.vue'
 
-const pinia = createPinia()
 const app = createApp(App)
 
-app.use(pinia)
 app.use(VueQueryPlugin)
 app.mount('#app')
